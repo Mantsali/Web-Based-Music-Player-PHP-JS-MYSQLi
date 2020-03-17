98b36a3fde3769a548c319e8f1719e81a9829328
Mantsali Nthabiseng Sekoli <mnsekoli@gmail.com>
2020-03-17 22:45:56 +0200

    Initital commit
    created a prototype layout

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DJ Allegro</title>
+    <link rel="stylesheet" href="css/thisstyle.css">
+    <script src="js/thisscript.js"></script>
+</head>
+<body>
+    <div class="card">
+        <h1>DJ Allegro</h1>
+        <p>Awesome tracks for your ears only</p>
+        <div class="player">
+
+        </div>
+        <div class="tracklist">
+            <ul>
+                <li><p>15-blink-182-remember_to_forget_me.mp3</p><p>Download</p></li>
+                <li>Tw15-blink-182-remember_to_forget_me.mp3o</li>
+                <li>Th15-blink-182-remember_to_forget_me.mp3ree</li>
+                <li>15-blink-182-remember_to_forget_me.mp3</li>
+                <li>F15-blink-182-remember_to_forget_me.mp3ive</li>
+                <li>S15-blink-182-remember_to_forget_me.mp3ix</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
