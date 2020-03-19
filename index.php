0b71525a25b26594a59c2e9840ee8487047fab47
Mantsali Nthabiseng Sekoli <mnsekoli@gmail.com>
2020-03-19 17:33:06 +0200

    Completed design layout
    Completed the desig still need to make it responsive

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!--
+Author: Mantsali N Sekoli
+Contact: mnsekoli@gmail.com
+github:
+
+--->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,23 +11,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DJ Allegro</title>
     <link rel="stylesheet" href="css/thisstyle.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <script src="js/thisscript.js"></script>
 </head>
 <body>
     <div class="card">
         <h1>DJ Allegro</h1>
-        <p>Awesome tracks for your ears only</p>
+        
         <div class="player">
-
+            <i class="fa fa-headphones" aria-hidden="true"></i>
         </div>
         <div class="tracklist">
             <ul>
-                <li><p>15-blink-182-remember_to_forget_me.mp3</p><p>Download</p></li>
-                <li>Tw15-blink-182-remember_to_forget_me.mp3o</li>
-                <li>Th15-blink-182-remember_to_forget_me.mp3ree</li>
-                <li>15-blink-182-remember_to_forget_me.mp3</li>
-                <li>F15-blink-182-remember_to_forget_me.mp3ive</li>
-                <li>S15-blink-182-remember_to_forget_me.mp3ix</li>
+                <li class="button">
+                    <div class="leftcol">15-blink-182-remember_to_forget_me.mp3</div>
+                    <div class="rightcol"><i class="fa fa-download"></i></div>
+                </li>
+                <li class="button">
+                    <div class="leftcol">15-blink-182-remember_to_forget_me.mp3</div>
+                    <div class="rightcol"><i class="fa fa-download"></i></div>
+                </li>
+                <li class="button">
+                    <div class="leftcol">15-blink-182-remember_to_forget_me.mp3</div>
+                    <div class="rightcol"><i class="fa fa-download"></i></div>
+                </li>
+                <li class="button">
+                    <div class="leftcol">15-blink-182-remember_to_forget_me.mp3</div>
+                    <div class="rightcol"><i class="fa fa-download"></i></div>
+                </li>
+                <li class="button">
+                    <div class="leftcol">15-blink-182-remember_to_forget_me.mp3</div>
+                    <div class="rightcol"><i class="fa fa-download"></i></div>
+                </li>
+                <li class="button">
+                    <div class="leftcol">15-blink-182-remember_to_forget_me.mp3</div>
+                    <div class="rightcol"><i class="fa fa-download"></i></div>
+                </li>
             </ul>
         </div>
     </div>
